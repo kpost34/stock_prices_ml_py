e51101e5f99346c56d099e66b132c34b57d77099
Keith <kpost342005@hotmail.com>
2024-08-24 17:31:21 -0600

    Updated calc_mae() such that option 'stock' is used to create variables 'y' and 'yhat' and created new fn calc_rmse() to calculate root mean squared errors

--- code/_00_helper_fns.py
+++ code/_00_helper_fns.py
@@ -3,13 +3,35 @@
 
 # Load Libraries====================================================================================
 import pandas as pd
+import math
 
 
 
 # Define Functions==================================================================================
-def calc_mae(df, y, yhat):
+## Calculate mean absolute error (MAE)
+def calc_mae(df, stock, places=2):
   n = len(df)
+  y = stock + '_adj_close'
+  yhat = stock + '_mean'
   
   s_diff_abs = abs(df[y] - df[yhat])
-  mae = (1/n) * sum(s_diff_abs)
+  mean_absolute_error = (1/n) * sum(s_diff_abs)
+  mae = round(mean_absolute_error, places)
   return mae
+
+
+
+## Calculate root mean squared error (RMSE)
+def calc_rmse(df, stock, places=2):
+  n = len(df)
+  y = stock + '_adj_close'
+  yhat = stock + '_mean'
+  
+  s_diff_sq = (df[y] - df[yhat])**2
+  root_mean_square_error = math.sqrt((1/n) * sum(s_diff_sq))
+  rmse = round(root_mean_square_error, places)
+  return rmse
+
+
+
+
