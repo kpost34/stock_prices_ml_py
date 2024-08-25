e03bce3800ca235292bcb6cb78cf72fd159cb440
Keith <kpost342005@hotmail.com>
2024-08-24 21:42:23 -0600

    Updated calc_mae() and calc_rmse() so that they returned DFs of the scores, means, and scores as percentsages of means

--- code/_00_helper_fns.py
+++ code/_00_helper_fns.py
@@ -10,27 +10,55 @@ import math
 # Define Functions==================================================================================
 ## Calculate mean absolute error (MAE)
 def calc_mae(df, stock, places=2):
+  #create objs
   n = len(df)
   y = stock + '_adj_close'
   yhat = stock + '_mean'
   
-  s_diff_abs = abs(df[y] - df[yhat])
-  mean_absolute_error = (1/n) * sum(s_diff_abs)
-  mae = round(mean_absolute_error, places)
-  return mae
+  #calculate mae
+  ser_diff_abs = abs(df[y] - df[yhat])
+  mae_raw = (1/n) * sum(ser_diff_abs)
+  mae = round(mae_raw, places)
+  
+  #calculate mean
+  mean_raw = sum(df[y])/n
+  mean = round(mean_raw, places)
+  
+  #calculate mae as percentage of mean
+  pct = round((mae_raw/mean_raw) * 100, places)
+  
+  #combine into DF
+  df_mae_mean_pct = pd.DataFrame({'mae': [mae], 'mean': [mean], 'mae_as_pct': [pct]}, 
+                                 index=[stock])
+                                 
+  return df_mae_mean_pct
 
 
 
 ## Calculate root mean squared error (RMSE)
 def calc_rmse(df, stock, places=2):
+  #create objs
   n = len(df)
   y = stock + '_adj_close'
   yhat = stock + '_mean'
   
-  s_diff_sq = (df[y] - df[yhat])**2
-  root_mean_square_error = math.sqrt((1/n) * sum(s_diff_sq))
-  rmse = round(root_mean_square_error, places)
-  return rmse
+  #calculate rmse
+  ser_diff_sq = (df[y] - df[yhat])**2
+  rmse_raw = math.sqrt((1/n) * sum(ser_diff_sq))
+  rmse = round(rmse_raw, places)
+  
+  #calculate mean
+  mean_raw = sum(df[y])/n
+  mean = round(mean_raw, places)
+  
+  #calculate rmse as percentage of mean
+  pct = round((rmse_raw/mean_raw) * 100, places)
+  
+  #combine into DF
+  df_rmse_mean_pct = pd.DataFrame({'rmse': [rmse], 'mean': [mean], 'rmse_as_pct': [pct]}, 
+                                  index=[stock])
+  
+  return df_rmse_mean_pct
 
 
 
